e7e4bab44faabe91e105534ed412eb18258c52d3
alyfazeha <alyfazeha@gmail.com>
2024-09-27 07:23:09 +0700

    berhasil

---
+++ Praktikum04/PemilihanBilangan05.java
@@ -0,0 +1,25 @@
+package Praktikum04;
+
+import java.util.Scanner;
+
+/**
+ * PemilihanBilangan05
+ */
+public class PemilihanBilangan05 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) 
+        {
+            System.out.println("Angka " + angka + " termasuk bilangan genap");    
+        }
+        else
+        {
+            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+        }
+    }
+}
